fb9ac2e0a7dfcc8bc989b4e2e6d3b8ac7c97818e
Lebanvla <antonlebeved2004@yandex.ru>
2023-10-19 12:47:53 +0300

    Добавил Putty и чуть-чуть 2 лабы

--- Lab_2/PHP/Header.php
+++ Lab_2/PHP/Header.php
@@ -1,87 +1,200 @@
-<div class="header">
-    <div class="header_line">
-        <div class="header_line_content">
-            <div class="header_line_section">
-                <div class="section-left">
-                    <div class="section-left-logo">
-                        <a href="#" class="section-logo-img"></a>
-                    </div>
-                    <ul class="section-left-menu">
-                        <li class="section-left-menu-li">
-                            <a href="#" class="section-left-menu-a">Блог</a>
-                        </li>
-                        <li class="section-left-menu-li">
-                            <a href="#" class="section-left-menu-a">Как заказать</a>
-                        </li>
-                        <li class="section-left-menu-li">
-                            <a href="#" class="section-left-menu-a">Возврат</a>
-                        </li>
-                        <li class="section-left-menu-li">
-                            <a href="#" class="section-left-menu-a">Акции</a>
-                        </li>
-                        <li class="section-left-menu-li">
-                            <a href="#" class="section-left-menu-a">Дилерам</a>
-                        </li>
-                        <li class="section-left-menu-li">
-                            <a href="#" class="section-left-menu-a">Где купить</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="section-right">
-                    <div class="section-right-content">
-                        <div class="section-right-content-span">
-                            Ваш город: Шуя
-                        </div>
-                    </div>
-                    <a href="#" class="section-right-content-a1">8 800 505-75-77</a>
-                    <a href="#" class="section-right-content-a2">gs@grass.su</a>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css" type = "text/css">
+    <link rel = "icon" href = "image/logos/grass_logo.png" type=”image/x-icon”>
+    <script src="library.js"></script>
+
+
+    <title>Grass.ru</title>
+</head>
+
+<header class="container px-5" >
+        <div class = "row col-12" style = "font-size: x-small;">
+            <div class=" col-2"  >
+                <a href = "#"><img src = "image/logos/logo.png"></a>
+            </div>
+            
+            <div class="   col-1"  >
+                <a href = "#" class="link-underline-light link-secondary link-underline-opacity-0">
+                    <div style = "font-size: x-small;">БЛОГ</div>
+                </a>
+            </div>
+
+            <div class="   col-1"  >
+                <a href = "#" class="link-underline-light link-secondary link-underline-opacity-0">
+                    <div style = "font-size: x-small;">КАК ЗАКАЗАТЬ</div>
+                </a>
+            </div>
+            
+            <div class="col-1"  >
+                <a href = "#" class="link-underline-light link-secondary link-underline-opacity-0">
+                    <div ">ВОЗВРАТ</div>
+                </a>
+            </div>
+
+            <div class="  col-1"  >
+                <a href = "#" class="link-underline-light link-secondary link-underline-opacity-0">
+                    <div ">АКЦИИ</div>
+                </a>
+            </div>
+
+            <div class="  col-1"  >
+                <a href = "#" class="link-underline-light link-secondary link-underline-opacity-0">
+                    <div ">ДИЛЕРАМ</div>
+                </a>
+            </div>
+        
+            <div class="  col-1"  >
+                <a href = "#" class="link-underline-light link-secondary link-underline-opacity-0">
+                    <div ">ГДЕ КУПИТЬ</div>
+                </a>
+            </div>
+
+            <div class=" col-1"></div>
+            
+
+            <div class ="  col-1"  >
+                <div style = "padding-top: 3%;">
+                Ваш город: 
+                <a href = "#" class="link-underline-light link-secondary link-underline-opacity-0">Москва</a>
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="header_line_two">
-        <div class="header_line_two_content">
-            <div class="header_line_two_section">
-                <div class="header_line_two_section1">
-                    <ul class="header_line_two_section1-ul">
-                        <li class="header_line_two_section1-li">
-                            <a href="#" class="header_line_two_section1-content-a">
-                                <div class="header_line_two_section1-content-a-span">
-                                    Каталог товаров
-                                </div>
-                            <div class="header_line_two_section1-content-a-burger"></div>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="header_line_two_section2">
-                    <div class="header_line_two_section2-content">
-                        <form action="/search/">
-                                <span class="header_line_two_section2-input-section2">
-                                        <input type="text" style = "width: 95%; margin-left: 30px;height: 40px; border-radius:10px;"  placeholder = "Искать на Grass" class="header_line_two_section2-input-i">
-                                </span>
-                        </form>
-                    </div>
-                </div>
-                <div class="header_line_two_section3">
-                    <div class="header_line_two_section3-content1">
-                        <a href="#" class="header_line_two_section3-content1-a">
-                            Вход и
-                            <br>
-                            Регистрация
-                        </a>
-                    </div>
-                    <div class="header_line_two_section3-content2">
-                        <a href="#" class="header_line_two_section3-content2-a">
-                            <span class="header_line_two_section3-content2-text">
-                                Корзина
-                                <br>
-                                пуста
-                            </span>
-                        </a>
-                    </div>
-                </div>
+        
+            <div class ="  col-1"  >
+                <a href = "#" class="link-underline-light link-secondary link-underline-opacity-5">
+                    <strong >8 800 505-77-77</strong>
+                </a>
             </div>
+
+            <div class ="  col-1"  >
+                <a href = "#"  class="link-underline-light link-secondary link-underline-opacity-0" style = "text-decoration-thickness: 1px;">
+                    <div>gs@grass.su</div>
+                </a>
+            </div>
+
+
+
+
+
         </div>
-    </div>
-</div>
+
+        <div class = "row col-12" style = "padding-top: 1%;">
+            <div class = "col-2 dropdown position:relative;">
+                <button class = " btn dropdown-toggle" styles = "background-color:#DAA520;" type="button" data-bs-toggle="dropdown" aria-expanded="false" id="katalog">
+                    <span style = "font-size: 0.75;">Каталог товаров</span>
+                    <img src = "image/logos/logo_from_button.png" style = "margin-left: 10%; ">
+                </button>
+                <ul class="dropdown-menu">
+                    <li>
+                        <button class="dropdown-item col-2">            
+                                Хиты продаж
+                        </button>
+                    </li>
+                    <li>
+                        <button class="dropdown-item col-2">
+                                Косметика MyHouse
+                        </button>
+                    </li>
+                    
+                    <li>
+                        <button class="dropdown-item col-2">
+                                Автохимия, Автокосметика
+                        </button>
+                    </li>
+                    
+                    <li>
+                        <button class="dropdown-item col-2">
+                                Детейлинг
+                    </li>
+
+                    <li>
+                        <button class="dropdown-item col-2">
+                                Бытовая химия
+                        </button>
+                    </li>
+
+                    <li>
+                        <button class="dropdown-item col-2">
+                                Бытовая химия
+                        </button>
+                    </li>
+
+                    <li>
+                        <button class="dropdown-item col-2">
+                                Клининг
+                        </button>
+                    </li>
+
+                    <li>
+                        <button class="dropdown-item col-2">
+                                Продукция для гостиниц
+                        </button>
+                    </li>
+
+                        <button class="dropdown-item col-2">
+                                Профессиональное оборудование для автомоек</button>
+                    </li>
+
+                    <li>
+                        <button class="dropdown-item col-2">
+                                Моющие средства для пищепрома</button>
+                    </li>
+
+                    <li>
+                        <button class="dropdown-item col-2">
+                                DutyBox</button>
+                    </li>
+
+                    <li>
+                        <button class="dropdown-item col-2">
+                                Промопродукция</button>
+                    </li>
+
+                    <li>
+                        <button class="dropdown-item col-2">
+                                Химия для бассейнов</button>
+                    </li>
+
+                    <li>
+                        <button class="dropdown-item col-2">
+                                Средства для профессиональных прачечных</button>
+                    </li>
+
+                    <li>
+                        <button class="dropdown-item col-2">
+                                Химия на розлив</button>
+                    </li>
+
+                    <li>
+                        <button class="dropdown-item col-2">
+                                Вместе выгоднее</button>
+                    </li>
+
+                    <li>
+                        <button class="dropdown-item col-2">
+                                Распродажа</button>
+                    </li>
+
+                </ul>
+            </div>
+            <div class = "col-8">
+                <input type = "text" class="finder form-control col-md-4 " placeholder = "Искать на Grass">
+            </div>
+            
+            <button class = "col-1 btn" style = "position: relative; border-color:white;">
+                <img src="image/logos/enter_to_site.png" style = "position: absolute; top: 5%; left: 0;"></icon>
+                <div class = "col-1" style = "font-size: x-small; padding-left: 30%;">Вход и регистрация</div>
+            </button>
+
+            <button class = "col-1 btn" style = "position: relative; border-color:white;">
+                <img  src="image/logos/container.png" style = "position: absolute; top: 20%; left: 0;"></icon>
+                <div class = "col-1" style = "font-size: x-small; padding-left: 40%;" >Корзина пуста</div>
+            </button>
+        
+        </div>
+</header>
+
---
+++ Lab_2/main.php
@@ -0,0 +1,182 @@
+<?php include 'PHP/header.php';?>
+    <main class = "container px-5">
+        <div class = "row col-10 text-center" > 
+            <a class = "col-1 p-1 link-offset-2 link-underline link-secondary link-underline-opacity-0" style = "font-size: 0.65em; " href = "#">Главная  >  </a>    
+            <a class = "col-1 p-1 link-offset-2 link-underline link-secondary link-underline-opacity-0" style = "font-size: 0.65em;" href = "#">Продукция  >  </a>   
+            <a class = "col-2 p-1 link-offset-2 link-underline link-secondary link-underline-opacity-0" style = "font-size: 0.65em;" href = "#">Автохимия, автокосметика  >  </a>   
+            <a class = "col-1 p-1 link-offset-3 link-underline link-secondary link-underline-opacity-0" style = "font-size: 0.65em;" href = "#">Мойка кузова  >  </a>   
+            <a class = "col-3 p-1 link-offset-2 link-underline link-secondary link-underline-opacity-0" style = "font-size: 0.65em;" href = "#">Активная пена, бесконтактная мойка  >  </a>   
+            <span class = "col-4 p-1 link-offset-2 link-underline link-secondary link-underline-opacity-0" style = "font-size: 0.65em;">Автошампунь, активная пена "Active Foam Pink" (канистра 6 кг)</span>   
+        </div>
+        
+            
+            <div class = "row col-12" >
+                <div class = "col-4">
+                    <img src="image/logos/skidka.png" class = "col-1 imgure" > 
+                    <span  class ="col-1" style = "font-size: 0.75em; padding-top: 5%; " > 
+                        Экономия: <strong id = "skidka" style = "color: #41AD0B">593.60</strong> 
+                    </span> <br>
+                    <img src = "image/main_picture_1_litr.png" class = "col-10">
+                </div>
+
+            <div class = "col-8" >
+                <h3>Автошампунь, активная пена "Active <br>Foam Pink" (канистра 1 л)</h3>
+                <div class = "row col-12">
+                        <div class = "col-7" style = "background-color: #f7fbf7;">
+                        <div class = "col-6">
+                            <div style = "font-size: 0.75em;">
+                                арт. 113120
+                            </div>
+                            <div>
+                                <img src = "image/logos/stars.png">
+                                <a class = "col-1 p-1 link-offset-2 link-underline link-secondary link-underline-opacity-0" style = "font-size: 0.65em; " href = "#">(<strong>28</strong>) Оставить отзыв</a>
+                            </div>
+                            <div>
+                                <span style = "font-size: 0.90em;">Цена:</span><br> 
+                                <span id = "price"><stong style = "font-size: 1.25em;">536.40</stong></span><strong>₽</strong>
+                            </div>
+                        </div>    
+                        <div class = "row col-12 text-center" style = "padding-left: 5%;">
+                            <span  class = "col-4 features_of_good have" >В наличии</span>
+                            <button class = "col-4 features_of_good" id = "favourites" style = " font-size: 0.65em; border: 0;">
+                                <strong>♡</strong> В избранное
+                            </button>
+                            <div class = "col-4 features_of_good" style = "border: 0; background-color: #41AD0B;"><a  class="link-light link-offset-2 link-underline-opacity-0 link-underline-opacity-0-hover" href = "#"  >Сравнение</a></div>
+                        </div>
+                        <br>
+                        <div class = "row col-9 px-1 need_border" style = "border: 1px #a1b1c5 solid; border-radius: 10px; margin-left: 5%;">
+                                <div class ="col-1">536,40₽</div> 
+                                <div class ="col-5"></div>
+                                <div class ="col-3" style = "font-size: 0.5em;">Оплата покупок<br> по частям</div>
+                                <img class = "little_image" src ="image/logos/podeli.png" >
+                                
+                                <br><br>
+                                <div class = "col-3" style = "font-size: 0.75em; color: #a1b1c5">Сегодня</div>
+                                <div class = "col-3" style = "font-size: 0.75em; color: #a1b1c5">02 ноября</div>
+                                <div class = "col-3" style = "font-size: 0.75em; color: #a1b1c5">16 ноября</div>
+                                <div class = "col-3" style = "font-size: 0.75em;color: #a1b1c5">30 ноября</div>
+                                <br>
+
+                                <div class = "col-3" style = "font-size: 0.75em;"><strong>134,10 ₽</strong></div>
+                                <div class = "col-3" style = "font-size: 0.75em;"><strong>134,10 ₽</strong></div>
+                                <div class = "col-3" style = "font-size: 0.75em;"><strong>134,10 ₽</strong></div>
+                                <div class = "col-3" style = "font-size: 0.75em;"><strong>134,10 ₽</strong></div>
+                                <br>
+                                <div class = "col-6" style = "font-size: 0.75em;">Без комиссий и переплат</div>
+                        </div>
+            
+                        <br><br>
+                        <button  class = "col-3 btn btn-outline-success features_of_good" style = "border: 1px; border-color: #41AD0B; padding-left: 5%;" >В корзину</button>
+                        <br><br>
+
+                        <div class = "row col-9" style = "padding-left: 5%;">
+                            <button id = "_1_litr" class="col-3 px-3 btn btn-success green_border little_round"  >1 л</button>
+                            <button id = "_6kg" class="col-3 px-3 btn btn-outline-success green_border little_round">6 кг</button>
+                            <button id = "_23kg " class="col-3 px-3 btn btn-outline-success green_border little_round">23.5 кг</button>
+                        </div>
+                        
+                        </div>
+                    <div class = "col-5 text-center" style = "font-size: 0.8em;">
+                            <img src = "image/logos/driver.png"><br>
+                            Доставка службами<br><a href = "#" class="link-success">Экспресс-доставки</a>
+                            <br>
+                            <img src = "image/logos/pay.png"><br>
+                            Оплата заказа:<br> <a href = "#" class="link-success">онлайн и другие способы</a>
+                    </div>
+                </div>
+                    <br>
+                        <ul class = "nav nav-tab text-center" data-toogle="tab">
+                            <li class = "col-2 " id = "description">
+                                <button class = "active"  >Описание</button> 
+                            </li>
+                            <li class = "col-2 " id = "characteristic">
+                                <button class = "nonactive" >Характеристика</button> 
+                            </li>
+                            <li class = "col-2 " id = "sertuficate">
+                                <button class = "nonactive" >Сертификаты</button> 
+                            </li>
+                        </ul>
+                        <br>
+                        <div id = "product_info">
+                            <div class = "row col-9">
+                                <div class = "col-5 little_text"><strong>Моющая способность </strong><span style="color:#41AD0B;" > ●●●●</span></div> 
+                                <div class = "col-1 little_text"></div>
+                                <div class = "col-3 little_text"><strong>Пенообразование</strong><span style="color:#41AD0B;" > ●●●●</span></div> 
+                            </div>
+                            <br>
+                            <div class = "row col-5 ">
+                                <div class = "little_text"><strong>Стойкость к жесткой воде</strong><span style="color:#41AD0B;" > ●●●○</span></div> 
+                            </div>
+
+
+                            <div class = "col-12 little_text">
+                                <br>
+                                Концентрированное моющее средство для бесконтактной мойки легкового и грузового транспорта. Эффективно удаляет дорожную грязь, пыль, масло, следы от насекомых. Легко смывается.<br>
+                                <br>
+                                <h6>Состав:</h6>
+                                ≥ 30% вода, ≥ 5%, но < 15% анионные ПАВ, неионогенные ПАВ, соль ЭДТА, гидроксид натрия, краситель.<br>
+                                <br>
+
+                                <h6>Способ применения:</h6>
+                                Перед нанесением средство необходимо разбавить с водой из расчета 1:5-1:10 (90-170г) в пенокомплект (1л) или 1:60-1:120 (8-17 г/л) для пеногенератора (25,50,100 л), в зависимости от степени загрязнения. При необходимости сбить верхний слой грязи. Остудить кузов автомобиля, после чего нанести разбавленный состав снизу вверх, чтобы избежать подтеков. Выдержать 1-2 мин. не допуская высыхания! Тщательно смыть водой под высоким давлением с близкого расстояния (15-20 см) снизу вверх.
+                                <br>
+                            </div>
+                            <br>
+                    </div>
+                
+                </div>
+                <ul class = "nav nav-tab text-center" data-toogle="tab">
+                    <li class = "col-3 " id = "description">
+                        <button class = "active" >Сопутствующие товары</button> 
+                    </li>
+                    <li class = "col-2 " id = "characteristic">
+                        <button class = "nonactive" >Аналоги</button> 
+                    </li>
+                </ul>
+                <br>
+                <div class = "row col-12 px-5">
+                    <div class = "col-2">
+                        <img class = "good_picture" src = "image/goods_photo/1.jpg">
+                        <h6>195.07₽</h6>
+                        <a class = "col-2 link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover very_little_text" href="#">Автошампунь "Auto Shampoo" с ароматом апельсина (флакон 500 мл)</a>
+                    </div>
+                    <div class = "col-2">
+                        <img class = "good_picture" src = "image/goods_photo/2.jpg">
+                        <h6>1 761₽</h6>
+                        <a class =  "col-2 link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover very_little_text" href="#" > Пенокомплект бытовой Karcher (пластик.)</a>
+                    </div>
+                    <div class = "col-2">
+                        <img class = "good_picture" src = "image/goods_photo/3.jpg">
+                        <h6>910₽</h6>
+                        <a class =  "col-2 link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover very_little_text" href="#"> Губка пенополиуретан "Автомобильная"</a>
+                    </div>
+                    <div class = "col-2">
+                        <img  class = "good_picture" src = "image/goods_photo/4.jpg">
+                        <h6>595.80₽</h6>
+                        <a class =  "col-2 link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover very_little_text" href="#"> Салфетка из искусственной замши 50х45см без лого 1сорт</a>
+                    </div>
+                    <div class = "col-2">
+                        <img class = "good_picture" src = "image/goods_photo/5.jpg">
+                        <h6>
+                            1 820₽</h6>
+                        <a class =  "col-2 link-secondary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover very_little_text" href="#"> Салфетка в тубе 43*32 см (эконом)</a>
+                    </div>
+
+                    
+                </div>
+    
+                    </div>
+                </div>
+            
+            </div>
+        </div>
+    </main>
+<br><br>
+<?php include 'PHP/footer.php'?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+    <script src = "main.js"></script>
+</body>
+<script src = "main.js"></script>
+
